89c881a4cc55edb9f671a2416a7151680b051623
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2022-09-06 21:17:58 +0300

    Align navbar/header items properly

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -5,21 +5,21 @@ export default function Header({ current }: { current?: string }) {
       <span
         className={`${
           current === "escp" ? "font-medium" : "invisible"
-        } mx-0.5 p-0.5`}
+        } mx-0.5 flex-1 p-0.5 text-left`}
       >
         escp.90
       </span>
       <span
         className={`${
           current === "booking" ? "font-medium" : "invisible"
-        } mx-0.5 p-0.5`}
+        } mx-0.5 flex-1 p-0.5 text-center`}
       >
         бронювання
       </span>
       <span
         className={`${
           current === "about" ? "font-medium" : "invisible"
-        } mx-0.5 p-0.5`}
+        } mx-0.5 flex-1 p-0.5 text-right`}
       >
         про студію
       </span>
