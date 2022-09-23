ea2a2926eb8068db8996ceb3ac6972cf12c2f23e
Denys Stetsenko <denys_stetsenko2@epam.com>
2022-09-23 11:24:22 +0300

    Change main page front images

--- app/routes/escp/index.tsx
+++ app/routes/escp/index.tsx
@@ -1,20 +1,28 @@
 import { useNavigate } from "@remix-run/react";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ActionButton } from "~/components/ActionButton/ActionButton";
 import Footer from "~/components/Footer/Footer";
 import Header from "~/components/Header/Header";
 import NavBar from "~/components/NavBar/NavBar";
+import { getIsMobile } from "~/utils/breakpoints";
 import { BOOKING_HOURLY_PRICE } from "~/utils/constants";
 
-// TODO: Use two adjacent photos
-const imageSrcFront = "https://i.imgur.com/vXQjup8.jpg";
-const imageSrcBack = "https://i.imgur.com/SLE4MrH.jpg";
+// desktop
+const imageSrcDesktopCollage = "https://i.imgur.com/MKN99RK.png";
+
+// mobile
+const imageSrcMobileMain = "https://i.imgur.com/KGpjCmh.png";
 
 export default function Escp() {
   const navigate = useNavigate();
   const navigateToBooking = useCallback(() => {
     navigate("/booking");
   }, [navigate]);
+
+  const [isMobile, setIsMobile] = useState(true);
+  useEffect(() => {
+    setIsMobile(getIsMobile());
+  }, []);
 
   return (
     <main className="flex min-h-screen w-full flex-col items-center p-4">
@@ -23,21 +31,26 @@ export default function Escp() {
       <div className="flex w-full flex-1 flex-col font-light">
         <Header current="escp" />
 
-        <div className="mx-auto mb-4 flex flex-col text-center sm:w-3/5">
-          <span>
-            <img
-              className="ml-auto mt-4 inline aspect-[3/2] w-fit bg-stone-100 xl:w-1/2"
-              src={imageSrcFront}
-              alt="Hall front"
-            />
-            <img
-              className="ml-auto mt-4 hidden aspect-[3/2] w-1/2 bg-stone-100 xl:inline"
-              src={imageSrcBack}
-              alt="Hall back"
-            />
-          </span>
+        <div className="mx-auto flex flex-col text-center sm:w-3/5">
+          {!isMobile ? (
+            <span>
+              <img
+                className="ml-auto inline aspect-[8/3] w-fit bg-stone-100"
+                src={imageSrcDesktopCollage}
+                alt="escp.90 instax collage"
+              />
+            </span>
+          ) : (
+            <span>
+              <img
+                className="ml-auto mt-4 inline aspect-[1/1] w-fit bg-stone-100"
+                src={imageSrcMobileMain}
+                alt="escp.90 instax main"
+              />
+            </span>
+          )}
 
-          <p className="mt-8 font-mono">Kyiv based photo studio / 90 m²</p>
+          <p className="font-mono">Kyiv based photo studio / 90 m²</p>
 
           <p className="mt-2 font-mono">
             вартість оренди: {BOOKING_HOURLY_PRICE} грн/год
