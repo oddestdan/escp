929a61a76a4871e26def5f2409da265ae7825885
Denys Stetsenko <denys_stetsenko2@epam.com>
2023-07-05 16:18:12 +0300

    fix SMS not arriving to studio 0 due to JS bullshit

--- app/models/smsNotificator.lib.ts
+++ app/models/smsNotificator.lib.ts
@@ -18,7 +18,7 @@ export const sendSMS = async (
     !roomName?.length ||
     !formattedDateString?.length ||
     !confirmationId?.length ||
-    !studioId
+    studioId === undefined
   ) {
     return;
   }
